11add238b5b305c570f0afd1da38901da825c295
wonseop <wonseop.s.lee@gmail.com>
2016-11-29 16:08:28 -0800

    detailsactivity

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineDetailActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaffeineDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_detail);
+    }
+}
